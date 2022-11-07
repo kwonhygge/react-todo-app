16ef6c3b32dfcb032b0ac7d6d11d1d6323b83c5f
kwonhygge <cleancode.kwon@gmail.com>
2022-11-07 10:14:20 +0900

    feat: login과 signup UI 마무리 및 유효성 체크 추가

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,11 +1,90 @@
 import React from "react";
+import { Button, TextField } from "@mui/material";
+import styled from "@emotion/styled";
+import { Controller, useForm } from "react-hook-form";
+import { EMAIL, PASSWORD } from "@/constants/name";
+import { RegexUtil } from "@/utils/regex";
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 function Login() {
+  const defaultValues: FormData = {
+    email: "",
+    password: "",
+  };
+
+  const { control, formState, handleSubmit } = useForm({
+    defaultValues,
+    mode: "onChange",
+  });
+  const { errors, isValid } = formState;
+
+  const onSubmit = () => {};
+
   return (
-    <div className="App">
-      <p>Login</p>
-    </div>
+    <form>
+      <StyledLogin>
+        <h2>Login</h2>
+        <Controller
+          control={control}
+          render={({ field: { name, onChange, value } }) => (
+            <StyledInputContainer>
+              <TextField
+                label={name}
+                name={name}
+                onChange={onChange}
+                error={!!errors?.[name]}
+                value={value}
+              />
+            </StyledInputContainer>
+          )}
+          rules={{
+            pattern: RegexUtil.REG_EMAIL,
+          }}
+          name={EMAIL}
+        />
+        <Controller
+          control={control}
+          render={({ field: { name, onChange, value } }) => (
+            <StyledInputContainer>
+              <TextField
+                label={name}
+                name={name}
+                onChange={onChange}
+                type={"password"}
+                error={!!errors?.[name]}
+                value={value}
+              />
+            </StyledInputContainer>
+          )}
+          rules={{
+            minLength: 8,
+          }}
+          name={PASSWORD}
+        />
+
+        <Button
+          disabled={!isValid}
+          variant={"contained"}
+          onClick={handleSubmit(onSubmit)}
+        >
+          Login
+        </Button>
+      </StyledLogin>
+    </form>
   );
 }
 
 export default Login;
+
+const StyledLogin = styled.div`
+  width: fit-content;
+  margin: 0 auto;
+`;
+
+const StyledInputContainer = styled.div`
+  margin: 0 0 10px;
+`;
--- client/src/pages/auth/SignUp.tsx
+++ client/src/pages/auth/SignUp.tsx
@@ -1,11 +1,114 @@
 import React from "react";
+import { Button, TextField } from "@mui/material";
+import styled from "@emotion/styled";
+import { Controller, useForm } from "react-hook-form";
+import { EMAIL, PASSWORD, PASSWORD_CONFIRM } from "@/constants/name";
+import { RegexUtil } from "@/utils/regex";
+
+interface FormData {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
 
 function SignUp() {
+  const defaultValues: FormData = {
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  };
+
+  const { control, formState, watch, handleSubmit } = useForm({
+    defaultValues,
+    mode: "onChange",
+  });
+  const { errors, isValid } = formState;
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <p>SignUp</p>
-    </div>
+    <form>
+      <StyledSignUp>
+        <h2>SignUp</h2>
+        <Controller
+          control={control}
+          render={({ field: { name, onChange, value } }) => (
+            <StyledInputContainer>
+              <TextField
+                label={name}
+                name={name}
+                onChange={onChange}
+                error={!!errors?.[name]}
+                value={value}
+              />
+            </StyledInputContainer>
+          )}
+          rules={{
+            pattern: RegexUtil.REG_EMAIL,
+          }}
+          name={EMAIL}
+        />
+        <Controller
+          control={control}
+          render={({ field: { name, onChange, value } }) => (
+            <StyledInputContainer>
+              <TextField
+                label={name}
+                name={name}
+                onChange={onChange}
+                type={"password"}
+                error={!!errors?.[name]}
+                value={value}
+              />
+            </StyledInputContainer>
+          )}
+          rules={{
+            minLength: 8,
+          }}
+          name={PASSWORD}
+        />
+        <Controller
+          control={control}
+          render={({ field: { name, onChange, value } }) => (
+            <StyledInputContainer>
+              <TextField
+                label={name}
+                name={name}
+                onChange={onChange}
+                type={"password"}
+                error={!!errors?.[name]}
+                value={value}
+              />
+            </StyledInputContainer>
+          )}
+          rules={{
+            minLength: 8,
+            validate: (value) => watch(PASSWORD) === value,
+          }}
+          name={PASSWORD_CONFIRM}
+        />
+
+        <Button
+          disabled={!isValid}
+          variant={"contained"}
+          onClick={handleSubmit(onSubmit)}
+        >
+          SignUp
+        </Button>
+      </StyledSignUp>
+    </form>
   );
 }
 
 export default SignUp;
+
+const StyledSignUp = styled.div`
+  width: fit-content;
+  margin: 0 auto;
+`;
+
+const StyledInputContainer = styled.div`
+  margin: 0 0 10px;
+`;
